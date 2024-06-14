773f95c855d14568d189b660080d840fe3c2d491
FilipKisic <filip.kisic@racunarstvo.hr>
2024-06-14 16:32:56 +0200

    Added unit tests for use cases

---
+++ src/test/kotlin/de/comsystoreply/gearbox/application/usecase/auth/UserSignInUseCaseTest.kt
@@ -0,0 +1,52 @@
+package de.comsystoreply.gearbox.application.usecase.auth
+
+import de.comsystoreply.gearbox.application.port.web.AuthenticationRequestDto
+import de.comsystoreply.gearbox.domain.user.model.User
+import de.comsystoreply.gearbox.domain.user.port.api.AuthenticationApiFacade
+import de.comsystoreply.gearbox.domain.user.port.api.PasswordPolicyViolationException
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class UserSignInUseCaseTest {
+
+    private lateinit var authenticationApiFacade: AuthenticationApiFacade
+    private lateinit var userSignInUseCase: UserSignInUseCase
+
+    @BeforeEach
+    fun setUp() {
+        authenticationApiFacade = mockk()
+        userSignInUseCase = UserSignInUseCase(authenticationApiFacade)
+    }
+
+    @Test
+    fun `sign in should return user on valid input`() {
+        val request = AuthenticationRequestDto(email = "test@example.com", password = "ValidPass123!")
+        val expectedUser = User("id", "test@example.com", "test", "ValidPass123!", null)
+        every { authenticationApiFacade.signIn(any(), any()) } returns expectedUser
+
+        val actualUser = userSignInUseCase.execute(request)
+
+        assertEquals(expectedUser, actualUser)
+        verify { authenticationApiFacade.signIn(expectedUser.email, expectedUser.password) }
+    }
+
+    @Test
+    fun `sign in should throw exception on invalid input`() {
+        val request = AuthenticationRequestDto(email = "test@example.com", password = "short")
+
+        every {
+            authenticationApiFacade.signIn(any(), any())
+        } throws PasswordPolicyViolationException("Password must have at least eight characters.")
+
+        val exception = assertThrows<PasswordPolicyViolationException> {
+            userSignInUseCase.execute(request)
+        }
+
+        assertEquals("Password must have at least eight characters.", exception.message)
+    }
+}
---
+++ src/test/kotlin/de/comsystoreply/gearbox/application/usecase/auth/UserSignUpUseCaseTest.kt
@@ -0,0 +1,64 @@
+package de.comsystoreply.gearbox.application.usecase.auth
+
+import de.comsystoreply.gearbox.application.port.web.AuthenticationRequestDto
+import de.comsystoreply.gearbox.domain.user.model.User
+import de.comsystoreply.gearbox.domain.user.port.api.AuthenticationApiFacade
+import de.comsystoreply.gearbox.domain.user.port.api.PasswordPolicyViolationException
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class UserSignUpUseCaseTest {
+
+    private lateinit var authenticationApiFacade: AuthenticationApiFacade
+    private lateinit var userSignUpUseCase: UserSignUpUseCase
+
+    @BeforeEach
+    fun setUp() {
+        authenticationApiFacade = mockk()
+        userSignUpUseCase = UserSignUpUseCase(authenticationApiFacade)
+    }
+
+    @Test
+    fun `signUp should return new user on valid input`() {
+        val request = AuthenticationRequestDto(
+            email = "test@example.com",
+            username = "test",
+            password = "ValidPass123!",
+            confirmPassword = "ValidPass123!"
+        )
+        val expectedUser = User("id", "test@example.com", "test", "ValidPass123!", null)
+
+        every { authenticationApiFacade.signUp(any()) } returns expectedUser
+
+        val actualUser = userSignUpUseCase.execute(request)
+
+        assertEquals(expectedUser, actualUser)
+        verify { authenticationApiFacade.signUp(any()) }
+    }
+
+    @Test
+    fun `signIn throw exception on invalid input`() {
+        val request = AuthenticationRequestDto(
+            email = "test@example.com",
+            username = "test",
+            password = "ValidPass123!",
+            confirmPassword = "ValidPass123!"
+        )
+
+        every {
+            authenticationApiFacade.signUp(any())
+        } throws PasswordPolicyViolationException("Password must have at least eight characters.")
+
+        val exception = assertThrows<PasswordPolicyViolationException> {
+            userSignUpUseCase.execute(request)
+        }
+
+        Assertions.assertEquals("Password must have at least eight characters.", exception.message)
+    }
+}
